a03025f789de472bb47fd8abfac557c5ede6a0a6
Ben Greene <benrgreene@gmail.com>
2018-10-23 12:38:52 -0700

    feat: update sticky header
    Header now is sticky when page has scrolled, allowing for updating
    header styles when page has been scrolled (height)

--- scripts/src/index.js
+++ scripts/src/index.js
@@ -1,13 +1,18 @@
 import './menu.js'
 // import 'search.js'
 
+const siteHeader  = document.querySelectorAll('.site-header')[0]
+const siteContent = document.querySelectorAll('.site-content')[0]
+
 // Basic theme setup
 document.addEventListener("DOMContentLoaded", (event) => {
   // Need to set the top padding of the content to match the size of the header
   setTimeout(function() {
-    let padding = document.querySelectorAll('.site-header')[0].clientHeight
+    let padding     = document.querySelectorAll('.site-header')[0].clientHeight
     let siteContent = document.querySelectorAll('.site-content')[0]
-    siteContent.style.paddingTop = `${padding}px`
+    let newStyle    = document.createElement('div')
+    newStyle.innerHTML = `<style>.site-content.fixed { padding-top: ${padding}px }</style>`
+    document.body.appendChild(newStyle)
   }, 10)
 
   // Setup for the container block attributes
@@ -16,4 +21,24 @@ document.addEventListener("DOMContentLoaded", (event) => {
     let containerType = block.getAttribute('container') || 'full-width'
     block.classList.add(containerType)
   })
+
+  // Now let's check if the page loaded NOT at the top (reload or something)
+  checkScrollPosition()
 })
+
+// On scroll, we'll check if we need to set the header to be sticky or not
+document.addEventListener("scroll", (event) => {
+  checkScrollPosition()
+})
+
+// check where the page is at and if the site header should be sticky
+function checkScrollPosition() {
+  let scrollPos = document.documentElement.scrollTop
+  if (5 > scrollPos) {
+    siteHeader.classList.remove('fixed')
+    siteContent.classList.remove('fixed')
+  } else {
+    siteHeader.classList.add('fixed')
+    siteContent.classList.add('fixed')
+  }
+}
